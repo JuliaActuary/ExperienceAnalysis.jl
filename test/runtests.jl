d20ca8246d007b96cedf42e7bb6b00cb5a53586b
Alec Loudenback <alecloudenback@gmail.com>
2020-10-19 06:36:40 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ using Dates
         @test exp[end] == (from = Date(2020, 1, 1), to = Date(2020, 1, 17))
     end
 
-    @testset "Year/Year" begin
+    @testset "Month/Year" begin
         exp = exposure(issue, termination, ExperienceAnalysis.PolicyCalendar(Month(1), Year(1)))
 
         @test exp[1]   == (from = Date(2016, 7, 4), to = Date(2016, 8, 4))
@@ -22,4 +22,12 @@ using Dates
         @test exp[7]   == (from = Date(2017, 1, 1), to = Date(2017, 1, 4))
         @test exp[end] == (from = Date(2020, 1, 4), to = Date(2020, 1, 17))
     end
+
+    @testset "Month/Month" begin
+    exp = exposure(issue, termination, ExperienceAnalysis.PolicyCalendar(Month(1), Month(1)))
+
+    @test exp[1]   == (from = Date(2016, 7, 4), to = Date(2016, 8, 1))
+    @test exp[2]   == (from = Date(2016, 8, 1), to = Date(2016, 8, 4))
+    @test exp[end] == (from = Date(2020, 1, 4), to = Date(2020, 1, 17))
+end
 end
